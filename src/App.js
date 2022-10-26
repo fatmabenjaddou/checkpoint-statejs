223522c73870dba4cab4eaadb518af68b3357968
fatma benjaddou <bjfatma943@gmail.com>
2022-10-26 12:41:31 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,45 +1,49 @@
+import "./App.css";
+import React, { Component } from "react";
+import Picture from "./fattouma.jpg";
+import Timer from "./component/Profile";
 
-import './App.css';
-import React, { Component } from 'react'
-import Picture from './cuty.png'
-import Timer from './component/Profile'
-
-
-class App extends React.Component {
+class App extends Component {
   state = {
     Person: {
-      fullName : "Ben jeddou Fatma",
-      bio : "Expert cyber security",
-      imgSrc : Picture,
-      profession : "Go my coder student"
+      fullName: "Ben jeddou Fatma",
+      bio: "Expert cyber security",
+      imgSrc: Picture,
+      profession: "STUDENT",
     },
-    show : false,
+    show: false,
   };
 
   render() {
-    var Handlechange = e =>
-    {
-      this.setState({show:!this.state.show})
-    }
-    const x=this.state.show;
+    var Handlechange = () => {
+      this.setState({
+        show: !this.state.show,
+      });
+    };
+
     return (
       <center>
-    <div>
-      
-      
-      <button onClick={Handlechange}>{x? 'hide' : 'show'}</button>
-      
-      <p className='faty'>
-      <h1>{this.state.Person.fullName}</h1>
-        <h2>{this.state.Person.bio}</h2>
-        <h2>{this.state.Person.profession}</h2>
-        </p>
-        <img src={Picture} alt="profile" />
+        <div>
+          <button onClick={Handlechange}>
+            {this.state.show ? "Hide" : "show"}
+          </button>
 
-      </div>
+          <p className="faty">
+            <Timer />
+            {this.state.show ? (
+              <div>
+                <h1>{this.state.Person.fullName}</h1>
+                <h2>{this.state.Person.bio}</h2>
+                <h3>{this.state.Person.profession}</h3>
+
+                <img src={Picture} alt="profile" />
+              </div>
+            ) : null}
+          </p>
+        </div>
       </center>
-    ); 
-    }
+    );
   }
+}
 
 export default App;
